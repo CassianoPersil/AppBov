972c467c1795bb612ac498ee25361817362cd681
Hiago Mendes <hiagomf@live.com>
2019-05-01 14:27:22 -0300

    Login Funcionando, inicio de preenchimento de informações

--- app/src/main/java/com/projetos/appbov/MainActivity.java
+++ app/src/main/java/com/projetos/appbov/MainActivity.java
@@ -5,9 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText usuario, senha;
     Button btLogar;
 
     @Override
@@ -15,12 +17,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Setando botões as variáveis
+        usuario = findViewById(R.id.ctEmail);
+        senha = findViewById(R.id.ctSenha);
+
+        usuario.setText("admin@login.com");
+        senha.setText("admin");
+
         btLogar = (Button)findViewById(R.id.btLogar);
         btLogar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getBaseContext(),MenudeAnimais.class);
-                startActivity(intent);
+                Login l = new Login();
+                if(l.logar(usuario.getText().toString(), senha.getText().toString())){
+                    Intent intent = new Intent(getBaseContext(),MenudeAnimais.class);
+                    startActivity(intent);
+                }else {
+                    Toast.makeText(getApplicationContext(),"Usuário ou senha incorretos", Toast.LENGTH_SHORT).show();
+                }
+
              }
         });
     }
